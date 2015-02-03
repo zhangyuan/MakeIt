737cffaaded1b89e0e16e2b3ba107da252b31e64
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-02-03 22:17:43 +0800

    add PostListTableViewController

---
+++ MakeIt/Controllers/PostListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostListTableViewController.h
+//  MakeIt
+//
+//  Created by zhangyuan on 2/3/15.
+//  Copyright (c) 2015 NextCloudMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostListTableViewController : UITableViewController
+
+@end
---
+++ MakeIt/Controllers/PostListTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  PostListTableViewController.m
+//  MakeIt
+//
+//  Created by zhangyuan on 2/3/15.
+//  Copyright (c) 2015 NextCloudMedia. All rights reserved.
+//
+
+#import "PostListTableViewController.h"
+
+@implementation PostListTableViewController
+
+@end
