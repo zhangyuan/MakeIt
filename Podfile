d0c19fd741ef884d8efbe7cd66b7ffcf2e675fac
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-02-03 20:50:19 +0800

    Add ZSSRichTextEditor

--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 
 pod 'AVOSCloud'
+pod 'ZSSRichTextEditor'
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -12,3 +12,10 @@
 #define COCOAPODS_VERSION_MINOR_AVOSCloud 6
 #define COCOAPODS_VERSION_PATCH_AVOSCloud 10
 
+// ZSSRichTextEditor
+#define COCOAPODS_POD_AVAILABLE_ZSSRichTextEditor
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.5.2.1.
+
