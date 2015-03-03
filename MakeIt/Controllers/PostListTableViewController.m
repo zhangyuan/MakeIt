a5fe1d49d08525ce629bb15ed648ce06475c69c5
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-03-04 00:57:41 +0800

    Add delete icon

--- MakeIt/Controllers/PostListTableViewController.m
+++ MakeIt/Controllers/PostListTableViewController.m
@@ -48,7 +48,7 @@
     NSMutableArray *rightUtilityButtons = [NSMutableArray new];
     [rightUtilityButtons sw_addUtilityButtonWithColor:
      [UIColor colorWithRed:1.0f green:0.231f blue:0.188 alpha:1.0f]
-                                                title:@"Delete"];
+                                                icon: [UIImage imageNamed:@"delete.png"]];
     
     return rightUtilityButtons;
 }
