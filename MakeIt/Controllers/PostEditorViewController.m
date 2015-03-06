f7013ced42c1e4e23a1eec4872a9440375b0b200
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-03-06 18:31:17 +0800

    Revert "do not formatHTML to avoid making html empty from pasting from wechat"
    This reverts commit a1c71cb1ca99bd73b77f991386efe2e0811ab276.

--- MakeIt/Controllers/PostEditorViewController.m
+++ MakeIt/Controllers/PostEditorViewController.m
@@ -17,7 +17,6 @@
 
 -(void) viewDidLoad {
     [super viewDidLoad];
-    self.formatHTML = NO;
     self.enabledToolbarItems = @[ZSSRichTextEditorToolbarBold, ZSSRichTextEditorToolbarTextColor, ZSSRichTextEditorToolbarInsertImage];
 }
 
