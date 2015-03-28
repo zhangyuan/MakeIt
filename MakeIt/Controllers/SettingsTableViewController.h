91cd7a28d3f4835c03a60223c47a80493a7c8aa9
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-03-28 19:57:43 +0800

    add sign out

---
+++ MakeIt/Controllers/SettingsTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingsTableViewController.h
+//  MakeIt
+//
+//  Created by zhangyuan on 3/28/15.
+//  Copyright (c) 2015 NextCloudMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UITableViewCell *signOutCell;
+
+@end
